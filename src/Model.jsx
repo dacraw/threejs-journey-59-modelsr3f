e8389f80df589cff3f0f76ff76bb55d75f3fe3ce
Doug <doug.a.crawford@gmail.com>
2024-03-16 02:08:38 -0700

    Use drei to load draco model

--- src/Model.jsx
+++ src/Model.jsx
@@ -1,7 +1,7 @@
 import { useGLTF } from "@react-three/drei";
 
 export default function Model() {
-  const model = useGLTF("./hamburger.glb");
+  const model = useGLTF("./hamburger-draco.glb");
 
   return <primitive object={model.scene} scale={0.35} />;
 }
